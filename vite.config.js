e5606a8c7e21968d724c9f0760776db6aeae6efb
dakshraut <138283515+dakshraut@users.noreply.github.com>
2026-08-05 02:33:05 +0530

    finally done

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "algorithm-visualizer",
+  base: "/algorithm-visualizer/",
   plugins: [
     tailwindcss(),
     react()
